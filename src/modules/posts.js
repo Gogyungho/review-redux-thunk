e964d62aecb8cd3189a66d607c2019d4adb9eb7b
Gogyungho <whohow10@gmail.com>
2021-09-17 01:15:49 +0900

    redux-thunk_handle promise

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts';
-import { reducerUtils } from '../lib/asyncUtils';
+import { reducerUtils, createPromiseThunk, handleAsyncActions } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -10,45 +10,49 @@ const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
 // thunk 함수들
-export const getPosts = () => async dispatch => {
-    //요청이 시작됨을 알림
-    dispatch({type: GET_POSTS});
-    //API 호출
-    try {
-        const posts = await postsAPI.getPosts();
-    //성공
-        dispatch({
-            type: GET_POSTS_SUCCESS,
-            posts,
-        });
-    } catch(e){
-    //실패
-        dispatch({
-            type: GET_POSTS_ERROR,
-            error: e,
-        })
-    }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 
-export const getPost = id => async dispatch => {
-    //요청이 시작됨을 알림
-    dispatch({type: GET_POST});
-    //API 호출
-    try {
-        const posts = await postsAPI.getPost(id);
-    //성공
-        dispatch({
-            type: GET_POST_SUCCESS,
-            posts,
-        });
-    } catch(e){
-    //실패
-        dispatch({
-            type: GET_POST_ERROR,
-            error: e,
-        })
-    }
-};
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+// export const getPosts = () => async dispatch => {
+//     //요청이 시작됨을 알림
+//     dispatch({type: GET_POSTS});
+//     //API 호출
+//     try {
+//         const posts = await postsAPI.getPosts();
+//     //성공
+//         dispatch({
+//             type: GET_POSTS_SUCCESS,
+//             posts,
+//         });
+//     } catch(e){
+//     //실패
+//         dispatch({
+//             type: GET_POSTS_ERROR,
+//             error: e,
+//         })
+//     }
+// };
+
+// export const getPost = id => async dispatch => {
+//     //요청이 시작됨을 알림
+//     dispatch({type: GET_POST});
+//     //API 호출
+//     try {
+//         const posts = await postsAPI.getPostById(id);
+//     //성공
+//         dispatch({
+//             type: GET_POST_SUCCESS,
+//             posts,
+//         });
+//     } catch(e){
+//     //실패
+//         dispatch({
+//             type: GET_POST_ERROR,
+//             error: e,
+//         })
+//     }
+// };
 
 // reducer
 
@@ -56,40 +60,59 @@ const initialState = {
     posts: reducerUtils.initial(),
     post: reducerUtils.initial()
 };
+
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+
 export default function posts(state=initialState, action){
     switch(action.type){
         case GET_POSTS:
-        return {
-            ...state,
-            posts: reducerUtils.loading(),
-        };
         case GET_POSTS_SUCCESS:
-            return {
-                ...state,
-                posts: reducerUtils.success(action.posts),
-            };
         case GET_POSTS_ERROR:
-            return {
-                ...state,
-                posts: reducerUtils.error(action.error),
-            };
+            return getPostsReducer(state, action);
         case GET_POST:
-            return {
-                ...state,
-                post: reducerUtils.loading(),
-            };
         case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post: reducerUtils.success(action.post),
-            };
         case GET_POST_ERROR:
-            return {
-                ...state,
-                post: reducerUtils.error(action.error),
-            };
-        
+            return getPostReducer(state, action);
         default: 
         return state;
     };
 };
+
+// export default function posts(state=initialState, action){
+//     switch(action.type){
+//         case GET_POSTS:
+//         return {
+//             ...state,
+//             posts: reducerUtils.loading(),
+//         };
+//         case GET_POSTS_SUCCESS:
+//             return {
+//                 ...state,
+//                 posts: reducerUtils.success(action.payload),
+//             };
+//         case GET_POSTS_ERROR:
+//             return {
+//                 ...state,
+//                 posts: reducerUtils.error(action.payload),
+//             };
+//         case GET_POST:
+//             return {
+//                 ...state,
+//                 post: reducerUtils.loading(),
+//             };
+//         case GET_POST_SUCCESS:
+//             return {
+//                 ...state,
+//                 post: reducerUtils.success(action.payload),
+//             };
+//         case GET_POST_ERROR:
+//             return {
+//                 ...state,
+//                 post: reducerUtils.error(action.payload),
+//             };
+        
+//         default: 
+//         return state;
+//     };
+// };
